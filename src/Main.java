a3a3b59a0644b1b3639428308878650a9dd29966
Chyngyz <chyngyzturapov@gmail.com>
2020-10-04 14:48:28 +0600

    up

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,20 @@ public class Main {
         System.out.println("Информация о боссе: " + boss1.getTypeOfDefence() + " "
                 + boss1.getBossDamage() + " " + boss1.getBossHealth());
 
-        private static Hero createHeroes () {
-            Hero hero1 = new Hero(20, 100, "IronArms");
-            Hero hero2 = new Hero(15, 90, "Teleportation");
-            Hero hero3 = new Hero(18, 95, "FrostBite");
-
-            Hero[] allHeroes = {hero1, hero2, hero3};
-            return;
+        for (int i = 0; i < createHeroes().length; i++) {
+            System.out.println("Heal Points: " + createHeroes()[i].getHeroesHealth() +  ", Damage: " +
+                    createHeroes()[i].getHeroesDamage() + ", Superpower: " + createHeroes()[i].getSuperpower());
         }
-        System.out.println(allHeroes);
+    }
 
+        private static Hero[] createHeroes () {
+            Hero Axe = new Hero(20, 100, "IronArms");
+            Hero Antimag = new Hero(15, 90, "Teleportation");
+            Hero Rylai = new Hero(18, 95, "FrostBite");
+
+            Hero[] allHeroes = {Axe, Antimag, Rylai};
+            return allHeroes;
+        }
 
     }
 
-}
